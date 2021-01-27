c69f17b8fcdab5ed0eca331691cbe34c83fae44c
alimirjahani <ali.mirjahani@cafebazaar.ir>
2021-01-27 23:08:33 +0330

    parmiss force push

--- twitterapp/models.py
+++ twitterapp/models.py
@@ -5,15 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Tweet(models.Model):
     id = models.AutoField(primary_key=True)
-    text = models.CharField(max_length=280)
-    username = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
+    text=models.CharField(max_length=280, default="")
+    user_id = models.CharField(max_length=280, default="")
+    date= models.DateTimeField(auto_now_add=True)
 class Account(User):
     profile_image = models.ImageField(blank=True)
     bio = models.CharField(max_length=280, default="")
--- twitterapp/urls.py
+++ twitterapp/urls.py
@@ -1,19 +1,22 @@
+
 from django.urls import path
 # from .views import tweet_list 
 # from .views import TweetAPIView,TweetDetails,
-from .views import GenericAPIView, RegisterApi, UserAPIView, FollowAPIView
+from .views import GenericAPIView,RegisterApi,UserAPIView,FollowAPIView,uploadProfileImageAPI
 from rest_framework_simplejwt import views as jwt_views
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     # path('tweets/',TweetAPIView.as_view()),
     # path('tweet/<int:pk>/',TweetDetails.as_view()),
-    path('generic/tweet/<int:pk>/', GenericAPIView.as_view()),
-    path('generic/tweet/', GenericAPIView.as_view()),
+    path('generic/tweet/<int:pk>/',GenericAPIView.as_view()),
+    path('generic/tweet/',GenericAPIView.as_view()),
     path('generic/register/', RegisterApi.as_view()),
     path('generic/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('generic/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('generic/follow/', FollowAPIView.as_view()),
-    path('generic/user/', UserAPIView.as_view())
+    path('generic/follow/',FollowAPIView.as_view()),
+    path('generic/user/',UserAPIView.as_view()),
+    path('generic/upload_profile/',uploadProfileImageAPI.as_view())
     # path('tweets/', tweet_list ),
     # path('tweet/<int:pk>/',tweetdetails)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
