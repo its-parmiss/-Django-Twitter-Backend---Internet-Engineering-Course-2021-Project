1929d48b423603b5b70bf3b4ca93333a5a62dbd6
its-parmiss <parmiss.sh98@gmail.com>
2021-01-27 13:04:47 +0330

    POST

--- twitterapp/views.py
+++ twitterapp/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import Tweet
 from .serializers import TweetSerializer
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
 def tweet_list(request):
     if request.method =='GET':
         tweets= Tweet.objects.all()
         serializers=TweetSerializer(tweets,many=True)
         return JsonResponse(serializers.data,safe=False)
-    elif request.method =='Post':
+    elif request.method =='POST':
         data = JSONParser().parse(request)
         serializers = TweetSerializer(data=data)
 
