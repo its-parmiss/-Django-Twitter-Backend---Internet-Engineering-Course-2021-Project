40443f14529f78b87ee332d86ee37b64a4cc6a16
alimirjahani <ali.mirjahani@cafebazaar.ir>
2021-01-27 20:20:19 +0330

    add models to admin

--- twitterapp/admin.py
+++ twitterapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Tweet
+from .models import Tweet, Account, UserFollowing
+
 admin.site.register(Tweet)
+admin.site.register(Account)
+admin.site.register(UserFollowing)
 # Register your models here.
