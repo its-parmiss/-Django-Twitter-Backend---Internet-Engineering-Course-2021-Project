b79bef908c93d22aa48964898a20afc0f7852b81
alimirjahani <ali.mirjahani@cafebazaar.ir>
2021-01-28 04:12:05 +0330

    fix token fast expire

--- twitter/settings.py
+++ twitter/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,7 @@ STATIC_URL = "/static/"
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:8001',
                         'http://localhost:8080']
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=5)
+}
